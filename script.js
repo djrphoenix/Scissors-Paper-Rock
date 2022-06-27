8f8d1291e85cbcda15415338c2c3fc9f0f9b6bc5
djrphoenix <dylan_4@live.com.au>
2022-06-27 18:36:17 +1000

    Scissors/paper/rock now lower case & draw function

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 function computerPlay() {
     // game choice 
-    const game = ['Scissors', 'Paper', 'Rock']
+    const game = ['scissors', 'paper', 'rock']
     // random variable number which spits out 0,1,2 according to length of above array
     let choice = Math.floor(Math.random() * game.length);
     // aligning var number with their choices
     if (choice === 0) {
-        choice = 'Scissors'
+        choice = 'scissors'
     }
     else if (choice === 1) {
-        choice = 'Paper'
+        choice = 'paper'
     }
     else {
-        choice = 'Rock'
+        choice = 'rock'
     }
     // print to return a value
     console.log(choice);
 }
 
+function round(playerSelection, computerSelection) {
+
+    if (playerSelection === 'rock' && computerSelection == 'rock') {
+        console.log('Draw. You both picked Rock');
+    } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+        console.log('Draw. Ypu both picked Paper');
+    } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+        console.log('Draw. You both picked Scissors')
+    }
+}
