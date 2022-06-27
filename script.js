c5bb1ed10b84fe2d070edd8e7e9ce22bd5d54d25
djrphoenix <dylan_4@live.com.au>
2022-06-27 19:54:30 +1000

    Fleshed out all outcomes for a single round

--- script.js
+++ script.js
@@ -18,13 +18,27 @@ function computerPlay() {
 }
 
 function round(playerSelection, computerSelection) {
+
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
 
     if (player === 'rock' && computer == 'rock') {
         console.log('Draw. You both picked Rock');
-    } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+    } else if (player == 'paper' && computer == 'paper') {
         console.log('Draw. You both picked Paper');
-    } else if (playerSelection == 'scissors' && computerSelection == 'scissors')
+    } else if (player == 'scissors' && computer == 'scissors') {
         console.log('Draw. You both picked Scissors');
+    } else if (player === 'scissors' && computer === 'paper') {
+        console.log('You won! Scissors beats Paper');
+    } else if (player === 'scissors' && computer === 'rock') {
+        console.log('You lost! Rock beats paper');
+    } else if (player === 'paper' && computer === 'rock') {
+        console.log('You won! Paper beats Rock');
+    } else if (player === 'paper' && computer === 'scissors') {
+        console.log('You lost! Scissors beats Paper');
+    } else if (player === 'rock' && computer === 'scissors') {
+        console.log('You won! Rock beats Scissors');
+    } else if (player === 'rock' && computer === 'paper') {
+        console.log('You lost! Paper beats Rock');
+    }
 }
