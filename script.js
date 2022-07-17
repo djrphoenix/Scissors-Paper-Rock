a822b2a8fac26ae1a4bda125e6275b8e6c45a016
djrphoenix <dylan_4@live.com.au>
2022-07-17 18:24:05 +1000

    Updated to play on HTML with buttons

--- script.js
+++ script.js
@@ -15,11 +15,14 @@ function computerPlay() {
     }
 }
 
+// set the counter to zero to count score
+let playerScore = 0;
+let computerScore = 0;
+
 function round(playerSelection, computerSelection) {
 
     let player = playerSelection.toLowerCase();
     // let computer = computerSelection.toLowerCase();
-
 
     // changed return for alert to give detailed result
     if (player === 'rock' && computerSelection == 'rock') {
@@ -34,59 +37,79 @@ function round(playerSelection, computerSelection) {
     } else if (player === 'scissors' && computerSelection === 'paper') {
         winner = 'player'
         alert('You won! Scissors beats Paper');
+        playerScore++;
+        document.querySelector('.player').textContent = playerScore;
     } else if (player === 'scissors' && computerSelection === 'rock') {
         winner = 'computer';
         alert('You lost! Rock beats paper');
+        computerScore++;
+        document.querySelector('.computer').textContent = computerScore;
     } else if (player === 'paper' && computerSelection === 'rock') {
         winner = 'player';
         alert('You won! Paper beats Rock');
+        playerScore++;
+        document.querySelector('.player').textContent = playerScore;
     } else if (player === 'paper' && computerSelection === 'scissors') {
         winner = 'computer';
         alert('You lost! Scissors beats Paper');
+        computerScore++;
+        document.querySelector('.computer').textContent = computerScore;
     } else if (player === 'rock' && computerSelection === 'scissors') {
         winner = 'player';
         alert('You won! Rock beats Scissors');
+        playerScore++;
+        document.querySelector('.player').textContent = playerScore;
     } else if (player === 'rock' && computerSelection === 'paper') {
         winner = 'computer';
         alert('You lost! Paper beats Rock');
+        computerScore++;
+        document.querySelector('.computer').textContent = computerScore;
     }
+
+    if (playerScore > 4) {
+        document.querySelector('.score').textContent = 'RESULTS';
+        document.querySelector('.result').textContent = 'You beat the computer!';
+    }
+
+    if (computerScore > 4) {
+        document.querySelector('.score').textContent = 'RESULTS';
+        document.querySelector('.result').textContent = 'You beat the computer!';
+    }
+
+    if (playerScore > 4 || computerScore > 4) {
+        let btn = document.createElement("button");
+        btn.textContent = 'Play Again?';
+        let win = document.querySelector('.retry');
+        win.appendChild(btn);
+
+        function resetGame() {
+        btn.addEventListener('click', () => {
+            document.location.reload();
+        });
+        
+    }
+        resetGame();
+    }
+    
 }
 
-// const playerSelection = 'rock';
-// const computerSelection = computerPlay();
-// console.log(round(playerSelection, computerSelection));
+let scissors = document.querySelector('.scissors')
+scissors.addEventListener('click', () => {
+    const computerSelection = computerPlay();
+    const playerSelection = 'scissors';
+    round(playerSelection, computerSelection);
+})
 
+let paper = document.querySelector('.paper')
+paper.addEventListener('click', () => {
+    const computerSelection = computerPlay();
+    const playerSelection = 'paper';
+    round(playerSelection, computerSelection);
+})
 
-// set the counter to zero to count score
-let playerScore = 0;
-let computerScore = 0;
-
-
-// game function to run
-function game() {
-    // 5 game loop
-    for (let i = 0; i < 5; i++) {
-        // prompt function to get input
-        let playerSelection = prompt("Scissors, Paper or Rock?");
-        // gets computer guess
-        const computerSelection = computerPlay();
-        // calls a round to be played
-        round(playerSelection, computerSelection);
-        // increments score and logs a result
-        if (winner == 'player') {
-            playerScore++;
-            console.log('You have won this round!');
-        } else if (winner == 'computer') {
-            computerScore++;
-            console.log('You have lost this round!');
-        } else if (winner == 'draw')
-            console.log('Draw round')
-    }
-    // compares score once loop has been finalised
-    if (computerScore > playerScore) {
-        console.log('The computer wins!');
-    } else if (playerScore > computerScore) {
-        console.log('You beat the computer!');
-    } else if (computerScore == playerScore)
-        console.log('Draw game!');
-}
+let rock = document.querySelector('.rock')
+rock.addEventListener('click', () => {
+    const computerSelection = computerPlay();
+    const playerSelection = 'rock';
+    round(playerSelection, computerSelection);
+})
